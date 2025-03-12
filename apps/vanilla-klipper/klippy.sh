cc26d19691dfa016a52505f7d831f8394bfbde48
Julien Batonnet <julien.batonnet@gmail.com>
2025-03-12 10:20:17 -0400

    Vanilla Klipper printer customization

--- apps/vanilla-klipper/klippy.sh
+++ apps/vanilla-klipper/klippy.sh
@@ -5,11 +5,21 @@ python -m venv --without-pip $APP_ROOT
 source bin/activate
 
 # Prepare configuration
-cp /userdata/app/gk/printer_data/config/default/printer.${KOBRA_MODEL_CODE}_${KOBRA_VERSION}.cfg printer.cfg
-python /opt/rinkhals/scripts/process-cfg.py printer.cfg printer.generated.cfg
+KLIPPER_CONFIG=printer.klipper_${KOBRA_MODEL_CODE}.cfg
+if [ ! -f $KLIPPER_CONFIG ]; then
+    exit 1
+fi
+
+CONFIG_PATH=/userdata/app/gk/printer_data/config/printer.klipper.cfg
+if [ ! -f $CONFIG_PATH ]; then
+    sed '/-- SAVE_CONFIG --/,$d' /userdata/app/gk/printer.cfg > /tmp/printer.1.cfg
+    sed -n '/-- SAVE_CONFIG --/,$p' /userdata/app/gk/printer.cfg > /tmp/printer.2.cfg
+    python /opt/rinkhals/scripts/process-cfg.py /tmp/printer.1.cfg $KLIPPER_CONFIG printer.rinkhals.cfg > $CONFIG_PATH
+    cat /tmp/printer.2.cfg >> $CONFIG_PATH
+fi
 
 # Start Klippy
 cd klippy
-python -m klippy -a /tmp/unix_uds1 $APP_ROOT/printer.generated.cfg >> $RINKHALS_ROOT/logs/app-klippy.log 2>&1 &
+python -m klippy -a /tmp/unix_uds1 $CONFIG_PATH >> $RINKHALS_ROOT/logs/app-klippy.log 2>&1 &
 
 assert_by_name klippy
