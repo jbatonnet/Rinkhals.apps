c3baafdef837052d0f81e37c7aee6999f3e9636e
Julien Batonnet <julien.batonnet@gmail.com>
2025-03-22 20:48:21 -0400

    Removed vanilla Moonraker as it's going to be integrated into Rinkhals

--- apps/vanilla-moonraker/moonraker.sh
+++
@@ -1,8 +0,0 @@
-. /useremain/rinkhals/.current/tools.sh
-
-# Activate Python venv
-python -m venv --without-pip .
-. bin/activate
-
-# Start Klippy
-HOME=/userdata/app/gk python ./moonraker/moonraker/moonraker.py >> $RINKHALS_ROOT/logs/app-moonraker.log 2>&1 &
--- apps/vanilla-moonraker/moonraker/docs/pymdown-extras/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-setup(
-    name='pymdown_extras',
-    version='1.0.0',
-    py_modules=['collapse_code'],
-    install_requires=['pymdown-extensions>=10.7'],
-)
